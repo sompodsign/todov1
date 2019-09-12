f8db3276448d068b3b2771e23086e4ebf6dfa02a
sompodsign <47793716+sompodsign@users.noreply.github.com>
2019-09-13 01:56:05 +0600

    big change

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -17,30 +18,45 @@ app.use(express.static("public"));
 
 app.get('/', function(req, res) {
 
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-us", options);
+  let day = today.toLocaleDateString("en-us", options);
 
   res.render("list", {
-    day: day,
+    listTitle: day,
     newListItems: items
+  });
+
+  app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    if (req.body.list === "work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  });
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "work list",
+    newListItems: workItems
   });
 });
 
-
-
-app.post('/', function(req, res) {
-  var item = req.body.newItem;
-
-  items.push(item);
-  res.redirect("/");
+app.get("/about", function(req, res) {
+  res.render("about");
 });
+
 
 app.listen(3000, function() {
   console.log("server started on port 3000");
